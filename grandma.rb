020535804c5a28f194b879ae9bfef2630dda12d8
Sarah Rogers <s.rogers0715@yahoo.com>
2020-07-09 03:07:14 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,11 @@
 def speak_to_grandma (phrase)
-if phrase!=phrase.upcase
-!="I LOVE YOU TOO PUMPKIN"
-  puts "HUH?!SPEAK UP SONNY!"
-return "NO, NOT SINCE 1938!"
-if phrase == "I LOVE YOU GRANDMA!"
-  
+ if phrase!=phrase.upcase
+  "HUH?!SPEAK UP, SONNY!"
+elsif phrase==phrase.upcase
+ "NO, NOT SINCE 1938!"
+else phrase=="I LOVE YOU GRANDMA!"
+  "I LOVE YOU TOO PUMPKIN!"
+end
 end
 speak_to_grandma
 
